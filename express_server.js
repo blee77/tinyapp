250e104ae68448c3f2cba260d68c17835884cc07
Bruce Lee <brucehlee@yahoo.ca>
2023-03-03 09:50:46 -0500

    Updated URL Shortening (Part 1)

--- express_server.js
+++ express_server.js
@@ -2,12 +2,30 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/urls", (req,res) => {
+  const templeVars = {urls: urlDatabase};
+  res.render("urls_index", templeVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req,res) => {
+  const templeVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templeVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -20,6 +38,26 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
+let generateRandomString = () => {
+  const length = 6;
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    result += chars[randomIndex];
+  }
+  return result;
+};
+
+generateRandomString();
