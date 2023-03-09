fdf6b13af37e24443ce6011a469e687f401656d2
Bruce Lee <brucehlee@yahoo.ca>
2023-03-09 16:28:48 -0500

    Added Registering New Users cookies

--- express_server.js
+++ express_server.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
+
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser');
+
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
+//Middleware
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cookieParser());
-
-app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
-
+//view engine
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,38 +21,88 @@ const urlDatabase = {
 };
 
 
-app.get("/urls", (req,res) => {
-  const templeVars = {urls: urlDatabase, username: req.cookies['username']};
-  res.render("urls_index", templeVars);
-});
-
-
-
-app.get('/urls/new', (req, res) => {
-  const templateVars = {
-    username: req.cookies['username']
-  };
-  res.render('urls_new', templateVars);
-});
-
-
-app.get("/urls/:id", (req,res) => {
-  const templeVars = {id: req.params.id, longURL: urlDatabase[req.params.id] ,username: req.cookies['username']};
-  res.render("urls_show", templeVars);
-});
+//-----------------------------------------------------------
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 
 app.get("/register", (req,res) => {
   res.render("user_registration");
 });
 
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+  const userId = generateRandomString(); // generate random user ID
+
+  // Create new user object and add to global users object
+  const newUser = {
+    id: userId,
+    email,
+    password
+  };
+  users[userId] = newUser;
+
+  // Set user_id cookie with newly generated ID
+  res.cookie('user_id', userId);
+
+  // Redirect to /urls page
+  res.redirect('/urls');
+
+  // Debugging log to inspect users object
+  console.log(users);
+});
+//----------------------------------------------------------
+
+
+app.get("/urls", (req,res) => {
+  const userId = req.cookies["user_id"];
+  let userObj = null;
+  for (let user of Object.values(users)) {
+    if (user.id === userId) {
+      userObj = user;
+    }
+  }
+  console.log("75" ,userObj);
+  const templeVars = {urls: urlDatabase, user: userObj};
+  res.render("urls_index", templeVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  // const templateVars = {
+  //   username: req.cookies['username']
+  // };
+  const userId = req.cookies["user_id"];
+  let userObj = null;
+  for (let user of Object.values(users)) {
+    if (user.id === userId) {
+      userObj = user;
+    }
+  }
+  console.log("75" ,userObj);
+  const templateVars = {urls: urlDatabase, user: userObj};
+  res.render('urls_new', templateVars);
+});
+
+app.get("/urls/:id", (req,res) => {
+  const templeVars = {id: req.params.id, longURL: urlDatabase[req.params.id] ,username: req.cookies['username']};
+  res.render("urls_show", templeVars);
+});
 
 app.post('/login', (req, res) => {
   const { username } = req.body;
   res.cookie('username', username);
   res.redirect('/urls');
 });
-
 
 app.post('/logout', (req, res) => {
   const { username } = req.body;
